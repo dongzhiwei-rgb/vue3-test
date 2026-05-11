6f01a120fe2d2c50440a55c488db55649a11e5c6
dongzhiwei-rgb <2889723085@qq.com>
2026-05-11 17:28:13 +0800

    首次提交

---
+++ index.ts
@@ -0,0 +1,18 @@
+let home:string = 'js';
+
+let resume:string = `
+
+1. 拥有两年以上的前端开发经验，参与多个农业相关项目的研发工作。
+2. 熟练使用Echarts进行数据可视化大屏的开发。
+3. 熟练使用Vue2/3全家桶，es6语法， css3， h5，js，less，typescript， nodejs等。
+4. 熟练使用uniapp框架开发微信小程序。
+5. 熟练使用npm包管理器优化项目结构和依赖管理。
+6. 具备MySQL、SQL Server等数据库技术使用经验。
+7. 具备基于websocket和sse技术的前后端通信服务开发经验。
+8. 理解并实践过webpack的打包流程与原理，能够有效配置以满足项目需求。
+9. 前端基础扎实，UI界面还原精确，对前端性能优化有深入的理解和实操经验。
+10. 个人评价：逻辑思维能力强，工作态度认真负责，能够有效沟通，善于总结经验、独立解决技术难题。
+`
+export function getHome() {
+    return [home, resume];
+}
